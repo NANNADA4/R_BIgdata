882c74d3fb0d71d90281b2b8d37aa698e835c223
NANNADA4 <68773516+NANNADA4@users.noreply.github.com>
2020-10-21 12:02:42 +0900

    RDA

---
+++ RDA.R
@@ -0,0 +1,6 @@
+ID <- c(1, 2, 3, 4, 5)
+GENDER <- c("F", "M", "F", "M", "F")
+data_ex <- data.frame(ID = ID, GENDER = GENDER)
+
+save(data_ex, file = "./datas/data.rda")
+save(data_ex, file = "./datas/data.bigdata")
