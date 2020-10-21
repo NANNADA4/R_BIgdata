dc4c0e5e2769640c44b787ab48f75a5ad7e926f0
NANNADA4 <68773516+NANNADA4@users.noreply.github.com>
2020-10-21 12:35:11 +0900

    write csv, table

--- RDA.R
+++ RDA.R
@@ -1,9 +1,6 @@
-# ID <- c(1, 2, 3, 4, 5)
-# GENDER <- c("F", "M", "F", "M", "F")
-# data_ex <- data.frame(ID = ID, GENDER = GENDER)
+ID <- c(1, 2, 3, 4, 5)
+GENDER <- c("F", "M", "F", "M", "F")
+data_ex <- data.frame(ID = ID, GENDER = GENDER)
 
-# save(data_ex, file = "./datas/data.rda")
-# save(data_ex, file = "./datas/data.bigdata")
-
-load("./datas/data.rda")
-View(data_ex)
+write.csv(data_ex, "./datas/output.csv")
+write.table(data_ex, "./datas/output.txt")
