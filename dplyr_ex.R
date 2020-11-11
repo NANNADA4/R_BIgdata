91a241c08c43f5f1c6f65a9d808a1ca9fdfff2b6
NANNADA4 <coco96062233@gmail.com>
2020-11-11 12:53:47 +0900

    Add summarise()

--- dplyr_ex.R
+++ dplyr_ex.R
@@ -29,3 +29,25 @@ View(result9)
 
 result10 <- mutate(mtcars, level = ifelse(mpg > 20, "good", "normal"))
 View(result10)
+
+result11 <- distinct(mtcars, cyl)
+View(result11)
+
+result12 <- distinct(mtcars, am, cyl)
+View(result12)
+
+result13 <- summarise(mtcars, cyl_mean = mean(cyl), cyl_min = min(cyl), cyl_max = max(cyl), hp_median = median(hp), hp_sum = sum(hp))
+View(result13)
+
+grp_cyl <- group_by(mtcars, cyl)
+View(grp_cyl)
+result14 <- summarise(grp_cyl, mean(mpg))
+View(result14)
+
+sample1 <- sample_n(mtcars, 10)
+View(sample1)
+sample2 <- sample_n(mtcars, 10)
+View(sample2)
+
+sample3 <- sample_frac(mtcars, 0.2)
+View(sample3)
