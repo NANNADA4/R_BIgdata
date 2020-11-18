4b27763668ddb62c628cc4be11c3fd56656efee9
NANNADA4 <coco96062233@gmail.com>
2020-11-18 11:19:33 +0900

    left_join()

---
+++ join.R
@@ -0,0 +1,14 @@
+library(readxl)
+library(dplyr)
+
+m_data <- read_excel("./datas/07-2(1).xlsx")
+f_data <- read_excel("./datas/07-2(2).xlsx")
+
+join_data1 <- bind_rows(m_data, f_data)
+View(join_data1)
+
+data1 <- read_excel("./datas/07-3(1).xlsx")
+data2 <- read_excel("./datas/07-3(2).xlsx")
+
+join_data3 <- left_join(data1, data2, by = "ID")
+View(join_data3)
