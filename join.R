bbd0b3f827055f946b9ce070d151fc7e9a37884a
NANNADA4 <coco96062233@gmail.com>
2020-11-18 11:24:53 +0900

    inner_join()

--- join.R
+++ join.R
@@ -12,3 +12,6 @@ data2 <- read_excel("./datas/07-3(2).xlsx")
 
 join_data3 <- left_join(data1, data2, by = "ID")
 View(join_data3)
+
+join_data4 <- left_join(data1, data2, by = "ID")
+View(join_data4)
